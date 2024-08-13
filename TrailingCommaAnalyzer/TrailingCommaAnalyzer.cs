4953ce7c6b6c34c4b56e895028763ac304825a82
Leviathan <levi.mcgomes@gmail.com>
2024-08-13 14:19:49 +0100

    Removed localization for simplicity. This might be added back later if it proves valuable.

--- TrailingCommaAnalyzer/TrailingCommaAnalyzer.cs
+++ TrailingCommaAnalyzer/TrailingCommaAnalyzer.cs
@@ -21,21 +21,9 @@ namespace TrailingCommaAnalyzer
         // analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md
         // for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(
-            nameof(Resources.AnalyzerTitle),
-            Resources.ResourceManager,
-            typeof(Resources)
-        );
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(
-            nameof(Resources.AnalyzerMessageFormat),
-            Resources.ResourceManager,
-            typeof(Resources)
-        );
-        private static readonly LocalizableString Description = new LocalizableResourceString(
-            nameof(Resources.AnalyzerDescription),
-            Resources.ResourceManager,
-            typeof(Resources)
-        );
+        private static readonly string Title = "Missing trailing comma.";
+        private static readonly string MessageFormat = "Missing trailing comma.";
+        private static readonly string Description = "Trailing commas should always be added.";
         private const string Category = "Naming";
 
         private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
