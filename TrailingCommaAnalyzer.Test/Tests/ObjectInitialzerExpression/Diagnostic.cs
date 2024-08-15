20bb1555551fde793c3ef94ef1d10c79374b8c9b
Leviathan <levi.mcgomes@gmail.com>
2024-08-15 11:47:56 +0100

    Added test set for object initialzer expressions.

---
+++ TrailingCommaAnalyzer.Test/Tests/ObjectInitialzerExpression/Diagnostic.cs
@@ -0,0 +1,22 @@
+﻿namespace TrailingCommaAnalyzerTest
+{
+    internal class Program
+    {
+        struct MissingACommaStruct
+        {
+            public int A;
+            public int B;
+            public int C;
+        }
+
+        static void Main(string[] args)
+        {
+            var missingACommaStruct = new MissingACommaStruct
+            {
+                A = 10,
+                B = 20,
+                [|C = 30|]
+            };
+        }
+    }
+}
---
+++ TrailingCommaAnalyzer.Test/Tests/ObjectInitialzerExpression/Expected.cs
@@ -0,0 +1,22 @@
+namespace TrailingCommaAnalyzerTest
+{
+    internal class Program
+    {
+        struct MissingACommaStruct
+        {
+            public int A;
+            public int B;
+            public int C;
+        }
+
+        static void Main(string[] args)
+        {
+            var missingACommaStruct = new MissingACommaStruct
+            {
+                A = 10,
+                B = 20,
+                C = 30,
+            };
+        }
+    }
+}
