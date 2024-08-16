1f1d2cab99e01f7880f3dca8a61b9f840f72b62b
Leviathan <levi.mcgomes@gmail.com>
2024-08-16 13:21:14 +0100

    Added tests for SwitchExpression.

---
+++ TrailingCommaAnalyzer.Test/Tests/SwitchExpression/Diagnostic.cs
@@ -0,0 +1,15 @@
+﻿namespace TrailingCommaAnalyzerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var missingACommaStruct = 1 switch
+            {
+                1 => 10,
+                2 => 20,
+                [|3 => 30|]
+            };
+        }
+    }
+}
---
+++ TrailingCommaAnalyzer.Test/Tests/SwitchExpression/NoDiagnostic.cs
@@ -0,0 +1,10 @@
+﻿namespace TrailingCommaAnalyzerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var missingACommaStruct = 1 switch { 1 => 10, 2 => 20 };
+        }
+    }
+}
