293e496045fd4cb1a0126e1b93b571bb326fd80d
Leviathan <levi.mcgomes@gmail.com>
2024-08-16 13:43:40 +0100

    Added tests for ListPattern.

---
+++ TrailingCommaAnalyzer.Test/Tests/ListPattern/Diagnostic.cs
@@ -0,0 +1,20 @@
+﻿namespace TrailingCommaAnalyzerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a =
+            [
+                "This is a very long string",
+                "This is a very long string",
+                "This is a very long string",
+            ];
+            var missingAComma = a is [
+                "Short",
+                "Short",
+                [|"Short"|]
+            ];
+        }
+    }
+}
