e5db67d1b83d70145a777e64e2c42427e5fe2dbe
Leviathan <levi.mcgomes@gmail.com>
2024-08-16 13:36:37 +0100

    Added tests for PropertyPatternClause.

---
+++ TrailingCommaAnalyzer.Test/Tests/PropertyPatternClause/Expected.cs
@@ -0,0 +1,29 @@
+﻿namespace TrailingCommaAnalyzerTest
+{
+    internal class Program
+    {
+        struct MissingACommaStruct
+        {
+            public int A;
+            public int B;
+            public int C;
+        }
+
+        static void Main(string[] args)
+        {
+            var a = new MissingACommaStruct
+            {
+                A = 10,
+                B = 20,
+                C = 30,
+            };
+
+            var missingAComma = a is MissingACommaStruct
+            {
+                A: 10,
+                B: 20,
+                C: 30,
+            };
+        }
+    }
+}
