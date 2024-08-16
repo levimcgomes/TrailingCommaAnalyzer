51e359087f5c32d1f5638dc84cdd2c5ce9627cbb
Leviathan <levi.mcgomes@gmail.com>
2024-08-16 10:31:25 +0100

    Added tests for ArrayInitializerExpression.

---
+++ TrailingCommaAnalyzer.Test/Tests/ArrayInitializerExpression/Expected.cs
@@ -0,0 +1,15 @@
+﻿namespace TrailingCommaAnalyzerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var missingACommaStruct = new string[]
+            {
+                "This is a very long string",
+                "This is a very long string",
+                "This is a very long string",
+            };
+        }
+    }
+}
