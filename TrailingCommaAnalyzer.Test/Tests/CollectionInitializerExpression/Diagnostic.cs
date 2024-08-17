d9093437044fd1034ac9778a96896e35f426695e
Leviathan <levi.mcgomes@gmail.com>
2024-08-17 10:56:02 +0100

    Added tests for CollectionInitializerExpression.

---
+++ TrailingCommaAnalyzer.Test/Tests/CollectionInitializerExpression/Diagnostic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TrailingCommaAnalyzerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var missingACommaStruct = new List<string>
+            {
+                "This is a very long string",
+                "This is a very long string",
+                [|"This is a very long string"|]
+            };
+        }
+    }
+}
---
+++ TrailingCommaAnalyzer.Test/Tests/CollectionInitializerExpression/NoDiagnostic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TrailingCommaAnalyzerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var missingACommaStruct = new List<string> { "Short", "Short" };
+        }
+    }
+}
